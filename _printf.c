e978eed739a1d072d8039015e17dccdf9680e029
Abdulazeez Nuhu <nuhuabdul86@gmail.com>
2023-09-19 21:15:46 -0700

    updated code

--- _printf.c
+++ _printf.c
@@ -2,101 +2,85 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-
 /**
- * check_buffer_and_flush - check if buffer is full, then flush it
+ * check_buffer_and_flush - Check if the buffer is full and flush it if needed.
  *
- * @buffer: buffer array
- * @buffer_index: current buffer index
- *
- * Return: void
-*/
+ * @buffer: Buffer array
+ * @buffer_index: Current buffer index
+ */
 void check_buffer_and_flush(char *buffer, int *buffer_index)
 {
-	if (BUFFER_SIZE - 1 == *buffer_index)
+	if (*buffer_index == BUFFER_SIZE - 1)
 	{
-		write(1, buffer, BUFFER_SIZE);
+		write(1, buffer, *buffer_index);
 		*buffer_index = 0;
 	}
 }
 
 /**
- * add_string_to_buffer - add string characters to buffer
+ * _printf - Custom printf function that handles %, c, and s format specifiers.
  *
- * @buffer: buffer array
- * @buffer_index: current buffer index
- * @str: string to add
+ * @format: String format
  *
- * Return: the count of characters in the string
-*/
-int add_string_to_buffer(char *buffer, int *buffer_index, char *str)
-{
-	int count = 0;
-
-	while (*str != '\0')
-	{
-		buffer[(*buffer_index)++] = *str;
-		check_buffer_and_flush(buffer, buffer_index);
-		str++;
-		count++;
-	}
-	return (count);
-}
-
-/**
- * _printf - custom printf function
- *
- * @format: string format
- *
- * Return: the number of character for the format string
-*/
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	char buffer[BUFFER_SIZE], c, *str;
+	char buffer[BUFFER_SIZE], *str;
 	int buffer_index = 0, character_count = 0;
 
 	va_start(args, format);
 
-	/* loop through each character of the format string provided */
-	while (*format != '\0')
+	while (*format)
 	{
-		/* handle format specifier */
 		if (*format == '%')
 		{
 			format++;
 
 			switch (*format)
 			{
-			case '%':
-				buffer[buffer_index++] = '%';
-				check_buffer_and_flush(buffer, &buffer_index);
-				character_count++;
-				break;
+				case '%':
+					buffer[buffer_index++] = '%';
+					check_buffer_and_flush(buffer, &buffer_index);
+					character_count++;
+					break;
 
-			case 'c':
-				c = va_arg(args, int);
-				buffer[buffer_index++] = c;
-				check_buffer_and_flush(buffer, &buffer_index);
-				character_count++;
-				break;
+				case 'c':
+					buffer[buffer_index++] = va_arg(args, int);
+					check_buffer_and_flush(buffer, &buffer_index);
+					character_count++;
+					break;
 
-			case 's':
-				str = va_arg(args, char *);
-				character_count += add_string_to_buffer(buffer, &buffer_index, str);
-				break;
+				case 's':
+					str = va_arg(args, char *);
+					while (*str)
+					{
+						buffer[buffer_index++] = *str++;
+						check_buffer_and_flush(buffer, &buffer_index);
+						character_count++;
+					}
+					break;
+
+				default:
+					buffer[buffer_index++] = '%';
+					check_buffer_and_flush(buffer, &buffer_index);
+					character_count++;
+					break;
 			}
 		}
-		else /* handle ordinary characters */
+		else
 		{
 			buffer[buffer_index++] = *format;
 			check_buffer_and_flush(buffer, &buffer_index);
 			character_count++;
 		}
+
 		format++;
 	}
-	/* flush the remaining characters from buffer */
-	write(1, buffer, buffer_index++);
+
+	write(1, buffer, buffer_index);
 	va_end(args);
+
 	return (character_count);
 }
--- main.h
+++ main.h
@@ -4,5 +4,6 @@
 #define BUFFER_SIZE 1024
 
 int _printf(const char *format, ...);
+int add_string_to_buffer(char *buffer, int *buffer_index, char *str);
 
 #endif
